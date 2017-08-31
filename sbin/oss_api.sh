fa24e9247bd752317edb8beed12d77395023b55e
Peter Varkoly <peter@varkoly.de>
2017-08-31 21:05:10 +0200

    Script to communicate wirht api

---
+++ sbin/oss_api.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright 2017 Peter Varkoly <peter@varkoly.de>
+
+METHOD=$1
+CALL=$2
+DATA=$3
+if [ "$DATA" ]; then
+   DATA=" -d ${DATA}"
+fi
+
+TOKEN=$( grep de.openschoolserver.api.auth.localhost= /opt/oss-java/conf/oss-api.properties | sed 's/de.openschoolserver.api.auth.localhost=//' )
+curl -X $METHOD --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Authorization: Bearer '${TOKEN} $DATA "http://localhost:9080/api/$CALL"
+
+
