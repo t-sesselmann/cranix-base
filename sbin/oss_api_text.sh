c4810f83227baf0430e1d921d65cfc2165997238
Peter Varkoly <peter@varkoly.de>
2017-11-28 23:10:18 +0100

    Make api call silent.

--- sbin/oss_api_text.sh
+++ sbin/oss_api_text.sh
@@ -9,5 +9,5 @@ if [ "$DATA" ]; then
 fi
 
 TOKEN=$( grep de.openschoolserver.api.auth.localhost= /opt/oss-java/conf/oss-api.properties | sed 's/de.openschoolserver.api.auth.localhost=//' )
-curl -s -X $METHOD --header 'Content-Type: application/json' --header 'Accept: application/json' $DATA --header 'Authorization: Bearer '${TOKEN} "http://localhost:9080/api/$CALL"
+curl -s -X $METHOD --header 'Content-Type: application/json' --header 'Accept: text/plain' $DATA --header 'Authorization: Bearer '${TOKEN} "http://localhost:9080/api/$CALL"
 
