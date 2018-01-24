9d8719acb8e978d786b3ecc1b6e30adaa4451ce6
Peter Varkoly <varkoly@suse.de>
2018-01-24 14:44:07 +0100

    Make registering more robust.

--- tools/register.sh
+++ tools/register.sh
@@ -5,14 +5,26 @@ REPO_USER=${SCHOOL_REG_CODE:0:9}
 REPO_PASSWORD=${SCHOOL_REG_CODE:10:9}
 . /etc/os-release
 
+#Save the credentials
+echo "[${SCHOOL_UPDATE_URL}/${VERSION_ID}]
+username = ${REPO_USER}
+password = ${REPO_PASSWORD}
+
+[${SCHOOL_SALT_PKG_URL}]
+username = ${REPO_USER}
+password = ${REPO_PASSWORD}
+" > /etc/zypp/credentials.cat
+
+chmod 600 /etc/zypp/credentials.cat
+
 #Register salt-packages repository
 mkdir -p /srv/salt/repos.d/
-zypper  -D /srv/salt/repos.d/  rr 1 2 3 4 5 6 7 8 9 10 &> /dev/null
+zypper  -D /srv/salt/repos.d/  rr salt-packages &> /dev/null
 echo "[salt-packages]
 name=salt-packages
 enabled=1
 autorefresh=1
-baseurl=http://$REPO_USER:$REPO_PASSWORD@repo.cephalix.eu/${SALT_TESTING}salt-packages
+baseurl=${SCHOOL_SALT_PKG_URL}
 path=/
 type=rpm-md
 keeppackages=0
@@ -23,13 +35,13 @@ zypper -D /srv/salt/repos.d/ ar -G /tmp/salt-packages.repo
 zypper --gpg-auto-import-keys -D /srv/salt/repos.d/ ref
 
 #Register OSS repository
-zypper rr 1 2 3 4 5 6 7 8 9 10 &> /dev/null
+zypper rr OSS &> /dev/null
 
 echo "[OSS]
 name=OSS
 enabled=1
 autorefresh=1
-baseurl=http://$REPO_USER:$REPO_PASSWORD@repo.cephalix.eu/${OSS_TESTING}OSS/$VERSION_ID
+baseurl=${SCHOOL_UPDATE_URL}/$VERSION_ID
 path=/
 type=rpm-md
 keeppackages=0
@@ -39,13 +51,3 @@ zypper ar -G /tmp/oss.repo
 
 zypper --gpg-auto-import-keys ref
 
-echo "[http://repo.cephalix.eu/${OSS_TESTING}OSS/${VERSION_ID}]
-username = ${REPO_USER}
-password = ${REPO_PASSWORD}
-
-[http://repo.cephalix.eu/${SALT_TESTING}salt-packages]
-username = ${REPO_USER}
-password = ${REPO_PASSWORD}
-" > /etc/zypp/credentials.cat
-
-chmod 600 /etc/zypp/credentials.cat
