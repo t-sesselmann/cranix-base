18f3e8fd9547546cbde97e6d95e37bbfe18b5451
Peter Varkoly <varkoly@suse.de>
2018-01-24 14:42:31 +0100

    Use threads instead of fork.

--- sbin/oss_salt_event_watcher.py
+++ sbin/oss_salt_event_watcher.py
@@ -4,10 +4,21 @@
 #
 import fnmatch
 import subprocess
-import os
-
+import threading
 import salt.config
 import salt.utils.event
+
+def event_handler(ret):
+    if event_log_level == "debug":
+       print ret
+    if fnmatch.fnmatch(ret['tag'], 'salt/minion/*/start'):
+       print "Client started: " + ret['data']['id']
+       #Start the plugins
+       subprocess.call(["/usr/share/oss/plugins/client_plugin_handler.sh","start", ret['data']['id']])
+    if fnmatch.fnmatch(ret['tag'], 'salt/presence/change'):
+       if event_log_level == "debug":
+          #At the moment nothing to do
+          print(ret['data'])
 
 opts = salt.config.client_config('/etc/salt/master')
 
@@ -17,27 +28,19 @@ sevent = salt.utils.event.get_event(
         transport=opts['transport'],
         opts=opts)
 
+event_log_level="warning"
+if opts.has_key('oss_event_log_level'):
+   event_log_level = opts['oss_event_log_level']
+
 while True:
     ret = sevent.get_event(full=True)
     if ret is None:
         continue
 
     if fnmatch.fnmatch(ret['tag'], 'salt/event/exit'):
-	continue
-
-    newpid = os.fork()
-    if newpid == 0:
         continue
 
-    #TODO only for debug
-    print ret
-    if fnmatch.fnmatch(ret['tag'], 'salt/minion/*/start'):
-       #TODO only for verbose
-       print "Client started: " + ret['data']['id']
-       #Start the plugins
-       subprocess.call(["/usr/share/oss/plugins/client_plugin_handler.sh","start", ret['data']['id']])
+    t = threading.Thread(target=event_handler,args=(ret,))
+    t.start()
 
-    if fnmatch.fnmatch(ret['tag'], 'salt/presence/change'):
-       #At the moment nothing to do
-       print(ret['data'])
 
