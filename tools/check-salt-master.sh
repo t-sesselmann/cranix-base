b4e0311da40d85ea77a3378f807e62ddee3cc860
Peter Varkoly <peter@varkoly.de>
2018-02-28 07:05:04 +0100

    Fix slat master checking. Create all oss cron jobs as %config.

---
+++ tools/check-salt-master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+salt admin pkg.list_pkgs 2> /dev/null | grep 'Failed to authenticate'
+if [ $? = 0 ]; then
+        systemctl restart salt-master
+        systemctl restart oss_salt_event_watcher
+fi
+
