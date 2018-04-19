cff578d106825ac1fe0287445d26daea18d97157
Peter Varkoly <peter@varkoly.de>
2018-04-19 22:04:56 +0200

    Put login profile move registry patch into a separate file

--- plugins/shares/netlogon/open/oss-create-logon-script.sh
+++ plugins/shares/netlogon/open/oss-create-logon-script.sh
@@ -6,6 +6,7 @@ a=$3
 m=$4
 R=$5
 
+. /etc/sysconfig/schoolserver
 
 role=$( oss_api_text.sh GET users/byUid/$U/role )
 mkdir -p /var/lib/samba/sysvol/$R/scripts
@@ -18,6 +19,10 @@ else
 	cp /usr/share/oss/templates/login-default.bat /var/lib/samba/sysvol/$R/scripts/${U}.bat
 fi
 
+if [ ${SCHOOL_MOVE_PROFILE_TO_HOME} = "yes" -a ${SCHOOL_TEACHER_OBSERV_HOME} "no"  ]; then
+	cat /usr/share/oss/templates/login-profile-move-registy-patch >> /var/lib/samba/sysvol/$R/scripts/${U}.bat
+f
+
 defaultPrinter=$( oss_api.sh GET devices/byIP/$I/defaultPrinter )
 if [ "$defaultPrinter" ]; then
         printf "rundll32 printui.dll,PrintUIEntry /q /in /n \134\\printserver\\$defaultPrinter /j\"Default $defaultPrinter\"\r\n" >> /var/lib/samba/sysvol/$R/scripts/${U}.bat;
@@ -27,6 +32,7 @@ for printer in $( oss_api.sh GET devices/byIP/$I/availablePrinters )
 do
         printf "rundll32 printui.dll,PrintUIEntry /q /in /n \134\\printserver\\$printer /j\"$printer\"\r\n" >> /var/lib/samba/sysvol/$R/scripts/${U}.bat;
 done
+
 
 chown ${U} /var/lib/samba/sysvol/$R/scripts/${U}.bat
 setfacl -m m::rwx /var/lib/samba/sysvol/$R/scripts/${U}.bat
