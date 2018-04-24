184a895c2ed29fa4a6da273e5f68ba7602b59b76
Peter Varkoly <peter@varkoly.de>
2018-04-24 22:26:44 +0200

    Do not save update packages.

--- tools/read_installed_software.pl
+++ tools/read_installed_software.pl
@@ -10,14 +10,22 @@ my $count  = 0;
 my @pkgs   = `salt $minion pkg.list_pkgs`;
 my $name   = "";
 my $version = "";
-
+my $update  = 0;
 foreach(@pkgs) {
    if( $count > 1 ) {
 	if(  $count % 2 ) {
+                if( $update ) {
+                        $update = 0;
+                        next;
+                }
 		/\s+(.*)$/;
 		$version = uri_escape($1);
 		system("oss_api.sh PUT 'softwares/devicesByName/$hostname/$name/$version'");
 	} else {
+                if( /\s+Update for / ) {
+                        $update = 1;
+                        next;
+                }
 		/\s+(.*):/;
 		$name = uri_escape($1);
 	}
