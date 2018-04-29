038ef5420712caf77069c0a7102fde26d804417a
Peter Varkoly <peter@varkoly.de>
2018-04-29 18:38:11 +0200

    New functionality

--- salt/_modules/oss_client.py
+++ salt/_modules/oss_client.py
@@ -1,8 +1,11 @@
 import salt
+import os
 
+#Delivers the grains of the client
 def get_grains():
   return __grains__
 
+#Sets the controller ip adress
 def set_controller_ip(ip):
   if __grains__['os_family'] == 'Windows':
     text_file = open('c:\Windows\ClientControl\secret', 'w')
@@ -13,6 +16,7 @@ def set_controller_ip(ip):
   text_file.close()
   return True
 
+#Reads the controller ip adress
 def get_controller_ip():
   if __grains__['os_family'] == 'Windows':
     text_file = open('c:\Windows\ClientControl\secret', 'r')
@@ -22,3 +26,39 @@ def get_controller_ip():
   controller = text_file.read()
   text_file.close()
   return controller
+
+#Locks the client
+def lockClient():
+  if __grains__['os_family'] == 'Windows':
+    os.system('C:\Windows\ClientControl\ClientControl.exe lockClient')
+  else:
+    return True
+
+  return True
+
+#Unlocks the client
+def unLockClient():
+  if __grains__['os_family'] == 'Windows':
+    os.system("taskkill /IM lockClient.exe /F")
+    os.system("taskkill /IM blockInput.exe /F")
+  else:
+    return True
+  return True
+
+#Locks the input devices
+def lockInput():
+  if __grains__['os_family'] == 'Windows':
+    os.system('C:\Windows\ClientControl\ClientControl.exe blockInput')
+  else:
+    return True
+
+  return True
+
+#Unlocks the input devices
+def unLockInput():
+  if __grains__['os_family'] == 'Windows':
+    os.system("taskkill /IM blockInput.exe /F")
+  else:
+    return True
+
+  return True
