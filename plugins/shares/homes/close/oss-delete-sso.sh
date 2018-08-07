b48bd4ffe17e0f24fdddcb7ac2973e85cea9aed8
Peter Varkoly <peter@varkoly.de>
2018-08-07 20:52:17 +0200

    Implementing ALLOW_MULTIPLE_LOGIN

--- plugins/shares/homes/close/oss-delete-sso.sh
+++ plugins/shares/homes/close/oss-delete-sso.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 U=$1
 I=$2
 a=$3
@@ -8,4 +7,32 @@ R=$5
 
 
 oss_api.sh DELETE devices/loggedInUsers/$I/$U
+
+. /etc/sysconfig/schoolserver
+
+role=$( oss_api_text.sh GET users/byUid/$U/role )
+
+case "${role}" in
+        workstations)
+        ;;
+        students)
+                if [ "${SCHOOL_ALLOW_STUDENTS_MULTIPLE_LOGIN}" = "no" ]; then
+                        DN=$( oss_get_dn.sh ${U} )
+                fi
+        ;;
+        *)
+                if [ "${SCHOOL_ALLOW_MULTIPLE_LOGIN}" = "no" ]; then
+                        DN=$( oss_get_dn.sh ${U} )
+                fi
+esac
+
+if [ "${DN}" ]; then
+        tmpldif=$( mktemp /tmp/XXXXXXXX )
+        echo "${DN}" > ${tmpldif};
+        echo "changetype: modify
+delete: userWorkstations" >> ${tmpldif}
+        ldbmodify  -H /var/lib/samba/private/sam.ldb ${tmpldif}
+        rm -f ${tmpldif}
+
+fi
 
--- plugins/shares/homes/open/oss-make-sso.sh
+++ plugins/shares/homes/open/oss-make-sso.sh
@@ -9,3 +9,30 @@ R=$5
 
 oss_api.sh PUT devices/loggedInUsers/$I/$U
 
+. /etc/sysconfig/schoolserver
+role=$( oss_api_text.sh GET users/byUid/$U/role )
+
+case "${role}" in
+	workstations)
+	;;
+	students)
+		if [ "${SCHOOL_ALLOW_STUDENTS_MULTIPLE_LOGIN}" = "no" ]; then
+			DN=$( oss_get_dn.sh ${U} )
+		fi
+	;;
+	*)
+		if [ "${SCHOOL_ALLOW_MULTIPLE_LOGIN}" = "no" ]; then
+			DN=$( oss_get_dn.sh ${U} )
+		fi 
+esac
+
+if [ "${DN}" ]; then
+	tmpldif=$( mktemp /tmp/XXXXXXXX )
+	echo "${DN}" > ${tmpldif};
+	echo "changetype: modify
+add: userWorkstations
+userWorkstations: ${m}" >> ${tmpldif}
+        ldbmodify  -H /var/lib/samba/private/sam.ldb ${tmpldif}
+        rm -f ${tmpldif}
+
+fi
