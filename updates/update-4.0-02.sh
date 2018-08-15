ed20bbd924832fb801a00f08e1a101d92b2693d3
Peter Varkoly <peter@varkoly.de>
2018-08-15 20:57:38 +0200

    Modify database.

---
+++ updates/update-4.0-02.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ -e "/var/adm/oss/update-4.0-02" ]
+then
+echo "Patch 4.0-02 already installed"
+        exit 0
+fi
+
+if [ $( echo "select count(*) from Acls where group_id=1 and acl='system.dns'" | mysql OSS  | tail -n1 ) = 0 ]; then
+	echo "INSERT into Acls VALUES(NULL,NULL,1,'system.dns','Y',1);" | mysql OSS
+fi
+echo "DELETE From Enumerates WHERE name='roomControl' AND value='teachers';" | mysql OSS
+
+systemctl restart oss-api
+
+touch /var/adm/oss/update-4.0-02
+
