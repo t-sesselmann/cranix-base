7798e1294fc155877e4b0ed6d186cdf08cf6c3d1
Peter Varkoly <peter@varkoly.de>
2018-09-13 14:23:53 +0200

    Add tool for clean_up the userWorkstations attribute of a user.

--- tools/clean_up_userWorkstations.sh
+++ tools/clean_up_userWorkstations.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-ROLE=$1
+U=$1
 
 if [ -z "$ROLE" ]; then
-	echo 
-	echo "Usage: /usr/share/oss/tools/clean_up_userWorkstations.sh <role>"
+	echo
+	echo "Usage: /usr/share/oss/tools/clean_up_userWorkstations.sh <uid>"
 	echo
 	exit 1
 fi
-for U in $( oss_api.sh GET users/uidsByRole/${ROLE} )
-do
-        DN=$( oss_get_dn.sh ${U} )
-        if [ "${DN}" ]; then
-                tmpldif=$( mktemp /tmp/CleanUpWSXXXXXXXX )
-                echo "${DN}" > ${tmpldif};
-                echo "changetype: modify
+DN=$( oss_get_dn.sh ${U} )
+if [ "${DN}" ]; then
+        tmpldif=$( mktemp /tmp/CleanUpWSXXXXXXXX )
+        echo "${DN}" > ${tmpldif};
+        echo "changetype: modify
 delete: userWorkstations" >> ${tmpldif}
-                ldbmodify  -H /var/lib/samba/private/sam.ldb ${tmpldif}
-        fi
-done
+        ldbmodify  -H /var/lib/samba/private/sam.ldb ${tmpldif}
+fi
---
+++ tools/clean_up_userWorkstations_for_role.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ROLE=$1
+
+if [ -z "$ROLE" ]; then
+	echo
+	echo "Usage: /usr/share/oss/tools/clean_up_userWorkstations.sh <role>"
+	echo
+	exit 1
+fi
+for U in $( oss_api.sh GET users/uidsByRole/${ROLE} )
+do
+        DN=$( oss_get_dn.sh ${U} )
+        if [ "${DN}" ]; then
+                tmpldif=$( mktemp /tmp/CleanUpWSXXXXXXXX )
+                echo "${DN}" > ${tmpldif};
+                echo "changetype: modify
+delete: userWorkstations" >> ${tmpldif}
+                ldbmodify  -H /var/lib/samba/private/sam.ldb ${tmpldif}
+        fi
+done
