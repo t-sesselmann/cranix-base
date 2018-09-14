6149d41ae700a8a86754fa32be6bd8e0302d3acb
Peter Varkoly <peter@varkoly.de>
2018-09-14 13:15:11 +0200

    Do not clean up workstations userWorkstations attribute.

--- tools/clean_up_userWorkstations_for_all.sh
+++ tools/clean_up_userWorkstations_for_all.sh
@@ -2,5 +2,7 @@
 
 for p in $( oss_api_text.sh GET groups/text/byType/primary )
 do
-	/usr/share/oss/tools/clean_up_userWorkstations_for_role.sh $p
+	if [ ${p} != "workstations" ]; then
+		/usr/share/oss/tools/clean_up_userWorkstations_for_role.sh $p
+	fi
 done
