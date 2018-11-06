002eb9f0ddee3ce087ab25e1020da45ac0d1244c
Peter Varkoly <peter@varkoly.de>
2018-11-06 16:10:47 +0100

    Hardening reading the softwares.

--- tools/read_installed_software.py
+++ tools/read_installed_software.py
@@ -11,14 +11,23 @@ print hostname
 
 softwares=json.load(os.popen('salt --out=json '+minion+' pkg.list_pkgs'))
 
-for software in softwares[minion]:
-    if re.match(r'Update for ',software):
-        next
-    version=softwares[minion][software]
-    name=re.sub(version,"",software).strip()
-    new_file, filename = tempfile.mkstemp()
-    os.write(new_file,'{"name":"'+name+'","description":"'+software+'","version":"'+version+'"}')
-    os.close(new_file)
-    result=json.load(os.popen('/usr/sbin/oss_api_post_file.sh softwares/devicesByName/'+hostname+' '+filename))
-    os.remove(filename)
-
+if type(softwares[minion]) == dict:
+   for software in softwares[minion]:
+     if re.match(r'Update for ',software):
+           next
+     version=softwares[minion][software]
+     name=re.sub(version,"",software).strip()
+     new_file, filename = tempfile.mkstemp()
+     shash = {}
+     shash['name'] = u'' + name
+     shash['description'] = u'' + software
+     shash['version'] = version
+     try:
+        os.write(new_file,json.dumps(shash,sort_keys=True,ensure_ascii=False,encoding="utf-8"))
+     except UnicodeEncodeError:
+        print u'Could not write: ' + software
+     else:
+        os.close(new_file)
+        result=json.load(os.popen('/usr/sbin/oss_api_post_file.sh softwares/devicesByName/'+hostname+' '+filename))
+     finally:
+        os.remove(filename)
