8faffd1d3d1e38fab6af67392a42e20a5928c632
Peter Varkoly <peter@varkoly.de>
2018-11-14 11:44:02 +0100

    Fix the update script

--- sbin/oss_update.sh
+++ sbin/oss_update.sh
@@ -2,6 +2,5 @@ DATE=$( /usr/share/oss/tools/oss_date.sh )
 . /etc/profile.d/profile.sh
 echo "/var/log/OSS-UPDATE-$DATE" > /var/adm/oss/update-started
 zypper --no-gpg-checks --gpg-auto-import-keys -n up --auto-agree-with-licenses  2>&1 | tee /var/log/OSS-UPDATE-$DATE
-/etc/cron.daily/oss.list-updates
 rm /var/adm/oss/update-started
 
