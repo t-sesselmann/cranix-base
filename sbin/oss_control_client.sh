289fb81003b9dcec86788c74e7aaf14a98b71e07
Peter Varkoly <peter@varkoly.de>
2018-11-23 16:22:34 +0100

    Ticket#2018112067001309 — Anfrage oss-script

--- sbin/oss_control_client.sh
+++ sbin/oss_control_client.sh
@@ -3,5 +3,13 @@
 client=$1
 action=$2
 
-/usr/bin/curl "http://$client:1992/?action=$action"
+if [ -z "${client}" -o -z "${action}" ]; then
+	echo ""
+	echo "usage: oss_control_client.sh ClientName Action"
+	echo ""
+	echo "Actions: open close reboot shutdown wol logout unlockInput lockInput cleanUpLoggedIn"
+	echo ""
+	exit 1
+fi
+/usr/sbin/oss_api.sh PUT devices/byName/${client}/actions/${action}
 
