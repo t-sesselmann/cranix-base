005ff6c76865c40ee52be935d444b60913824ee8
Peter Varkoly <peter@varkoly.de>
2018-12-11 18:23:48 +0100

    Masquerade only for the default gateway.

--- sbin/oss_set_access_state.sh
+++ sbin/oss_set_access_state.sh
@@ -49,6 +49,7 @@ LOCAL=`/sbin/ip addr | grep "$DEST/"`
 
 case "$3" in
    direct)
+	DEV=$( ip route show | gawk '{ if( $1 == "default" ) { print $5 } }' )
 	if test "$1" = "1"; then
 		IFS=$'\n'; i=0; e=0; n=0;
 		for l in $( /usr/sbin/iptables -t filter -S forward_int ); do n=$((n+1)); echo $l | grep -q 'j ACCEPT' && i=$n; done;
@@ -58,13 +59,13 @@ case "$3" in
 		COMMAND="$COMMAND; /usr/sbin/iptables -I forward_ext $e -d $2 ! -s $NETWORK -j ACCEPT -m state --state RELATED,ESTABLISHED"
 		COMMAND="$COMMAND; /usr/sbin/iptables -I forward_int $i -s $2 ! -d $NETWORK -j ACCEPT -m state --state NEW,RELATED,ESTABLISHED"
 		COMMAND="$COMMAND; /usr/sbin/iptables -I forward_int $i -d $2 ! -s $NETWORK -j ACCEPT -m state --state RELATED,ESTABLISHED"
-		COMMAND="$COMMAND; /usr/sbin/iptables -I POSTROUTING -s $2 ! -d $NETWORK -j MASQUERADE -t nat;"
+		COMMAND="$COMMAND; /usr/sbin/iptables -I POSTROUTING -s $2 ! -d $NETWORK -o ${DEV} -j MASQUERADE -t nat;"
 	else
 		COMMAND="/usr/sbin/iptables -D forward_ext -s $2 ! -d $NETWORK -j ACCEPT -m state --state NEW,RELATED,ESTABLISHED &> /dev/null"
 		COMMAND="$COMMAND; /usr/sbin/iptables -D forward_ext -d $2 ! -s $NETWORK -j ACCEPT -m state --state RELATED,ESTABLISHED &> /dev/null"
 		COMMAND="$COMMAND; /usr/sbin/iptables -D forward_int -s $2 ! -d $NETWORK -j ACCEPT -m state --state NEW,RELATED,ESTABLISHED &> /dev/null"
 		COMMAND="$COMMAND; /usr/sbin/iptables -D forward_int -d $2 ! -s $NETWORK -j ACCEPT -m state --state RELATED,ESTABLISHED &> /dev/null"
-		COMMAND="$COMMAND; while /usr/sbin/iptables -D POSTROUTING -s $2 ! -d $NETWORK -j MASQUERADE -t nat ; do false; done &> /dev/null"
+		COMMAND="$COMMAND; while /usr/sbin/iptables -D POSTROUTING -s $2 ! -d $NETWORK -o ${DEV} -j MASQUERADE -t nat ; do false; done &> /dev/null"
 	fi
 	;;
    proxy|internet|printing|portal)
