82fd78cd51572b7dd07e883e8a7af5a5a2cc4992
Peter Varkoly <varkoly@suse.com>
2018-12-28 17:31:39 +0100

    Remove domain.

--- profiles/verbinden.bat
+++ profiles/verbinden.bat
@@ -3,8 +3,8 @@ set /p username="Benutzername: "
 
 set /p password="Passwort: " 
 
-net use Z: \\admin\%username% /persisten:no /user:thrintern\%username% %password% 
-net use P: \\admin\all /persisten:no /user:thrintern\%username% %password%
-net use M: \\admin\groups /persisten:no /user:thrintern\%username% %password%
-net use S: \\admin\software /persisten:no /user:thrintern\%username% %password%
+net use Z: \\admin\%username% /persisten:no /user:%username% %password% 
+net use P: \\admin\all /persisten:no /user:%username% %password%
+net use M: \\admin\groups /persisten:no /user:%username% %password%
+net use S: \\admin\software /persisten:no /user:%username% %password%
 
