6b244d0ac6502595649faa79e4dc21edb9bc9e39
Peter Varkoly <varkoly@suse.com>
2019-01-05 17:00:11 +0100

    New tool to check and set the services.

---
+++ tools/check_services.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+. /etc/sysconfig/schoolserver
+echo -n "["
+for i in ${SCHOOL_MONITOR_SERVICES}
+do
+  if [ "${NEXT}" ]; then
+    echo -n ","
+  fi
+  echo -n '{"service":"'$i'",'
+  enabled=$(systemctl is-enabled $i 2> /dev/null )
+  #if [ $? = 0 ]; then
+  #  echo -n '"status":"ok",'
+  #fi
+  if [ "${enabled}" = "enabled" ]; then
+    echo -n '"enabled":"true",'
+  else
+    echo -n '"enabled":"false",'
+  fi
+  active=$(systemctl is-active $i 2> /dev/null )
+  #if [ $? = 0 ]; then
+  #  echo -n '"status":"ok",'
+  #fi
+  if [ "${active}" = "active" ]; then
+    echo -n '"active":"true"'
+  else
+    echo -n '"active":"false"'
+  fi
+  echo -n "}"
+  NEXT=1
+done
+echo -n "]"
+
