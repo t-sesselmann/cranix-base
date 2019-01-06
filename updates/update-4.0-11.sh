eb74c45e47652ad897d3afc974c14fdc0cb4897a
Peter Varkoly <varkoly@suse.com>
2019-01-06 13:33:40 +0100

    Adapt SCHOOL_MONITOR_SERVICES for the new api.

---
+++ updates/update-4.0-11.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Fix the workstation users. It is neccessary for some AD stuff.
+
+if [ -e "/var/adm/oss/update-4.0-11" ]
+then
+echo "Patch 4.0-11 already installed"
+        exit 0
+fi
+
+sed -i 's/^SCHOOL_MONITOR_SERVICES=.*/SCHOOL_MONITOR_SERVICES="amavis apache2 cups dhcpd ntp oss-api oss_salt_event_watcher postfix salt-master samba squid"/'  /etc/sysconfig/schoolserver
+
+touch /var/adm/oss/update-4.0-11
+
