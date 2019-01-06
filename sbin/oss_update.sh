f05a3fca8573e04bd13823aa248d55c757f26092
Peter Varkoly <varkoly@suse.com>
2019-01-06 23:19:06 +0100

    New release

--- sbin/oss_update.sh
+++ sbin/oss_update.sh
@@ -1,6 +1,6 @@
 DATE=$( /usr/share/oss/tools/oss_date.sh )
 . /etc/profile.d/profile.sh
 echo "/var/log/OSS-UPDATE-$DATE" > /var/adm/oss/update-started
-zypper --no-gpg-checks --gpg-auto-import-keys -n up --auto-agree-with-licenses  2>&1 | tee /var/log/OSS-UPDATE-$DATE
+zypper --no-gpg-checks --gpg-auto-import-keys -n up --auto-agree-with-licenses $@ 2>&1 | tee /var/log/OSS-UPDATE-$DATE
 rm /var/adm/oss/update-started
 
