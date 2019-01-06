643be9fd43e5da377c3ef1f97c486b4578039f0e
Peter Varkoly <varkoly@suse.com>
2019-01-06 13:40:52 +0100

    Add missed service too.

--- updates/update-4.0-11.sh
+++ updates/update-4.0-11.sh
@@ -7,7 +7,7 @@ echo "Patch 4.0-11 already installed"
         exit 0
 fi
 
-sed -i 's/^SCHOOL_MONITOR_SERVICES=.*/SCHOOL_MONITOR_SERVICES="amavis apache2 cups dhcpd ntp oss-api oss_salt_event_watcher postfix salt-master samba squid"/'  /etc/sysconfig/schoolserver
+sed -i 's/^SCHOOL_MONITOR_SERVICES=.*/SCHOOL_MONITOR_SERVICES="amavis apache2 cups dhcpd ntp oss-api oss_get_screenshots oss_salt_event_watcher postfix salt-master samba squid"/'  /etc/sysconfig/schoolserver
 
 touch /var/adm/oss/update-4.0-11
 
