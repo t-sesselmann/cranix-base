3c6b408225dece2cf87b66d968da73d0824bb045
Peter Varkoly <varkoly@suse.com>
2019-02-22 08:08:50 +0100

    Fix typo.

--- plugins/add_device/101-add-device.py
+++ plugins/add_device/101-add-device.py
@@ -35,7 +35,7 @@ elif netmask > 7:
   revdomain = network[0]+'.IN-ADDR.ARPA'
   rdomain = ip[3]+'.'+ip[2]+'.'+ip[1]
 
-if os.system("samba-tool dns add localhost " + revdomain + " " + rdomain + " PTR " + name + "." + domain + "  -U register%" + passwd ) != 0
+if os.system("samba-tool dns add localhost " + revdomain + " " + rdomain + " PTR " + name + "." + domain + "  -U register%" + passwd ) != 0:
   TASK = "/var/adm/oss/opentasks/101-add-device-" + os.popen('uuidgen -t').read().rstrip()
   with open(TASK, "w") as f:
     f.write("ip: "+','.join(ip))
