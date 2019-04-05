6b2fb42cf67dbb9ee1b352c3f863578b36ac1f5e
Peter Varkoly <varkoly@suse.com>
2019-04-05 13:48:58 +0200

    New release

---
+++ tools/get_printer_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import os
+import sys
+import re
+import json
+
+printers=[]
+printer={}
+name=""
+for line in os.popen('LANG=en_EN lpc status').readlines():
+  match = re.search("([\-\w]+):", line)
+  if match:
+     if "name" in printer:
+       if os.path.isfile("/var/lib/printserver/drivers/x64/3/"+name+".ppd"):
+         printer["windowsDriver"] = True
+       else:
+         printer["windowsDriver"] = False
+       printers.append(printer)
+     name =  match.group(1)
+     printer = { "name": name }
+     next
+  #Eval queuing
+  match = re.search("queuing is (\w+)", line)
+  if match:
+     if match.group(1) == "enabled":
+       printer["acceptingJobs"] = True
+     else:
+       printer["acceptingJobs"] = False
+     next
+
+  #Eval printing
+  match = re.search("printing is (\w+)", line)
+  if match:
+     printer["status"] = match.group(1)
+     next
+
+  #Eval jobs
+  match = re.search("(\w+) entries", line)
+  if match:
+     if match.group(1) == "no":
+       printer["activeJobs"] = 0
+     else:
+       printer["activeJobs"] = match.group(1)
+     next
+
+print json.dumps(printers)
