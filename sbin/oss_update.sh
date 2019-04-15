dc14b49f180f4de5373393d5077bff6a541e7cc6
Peter Varkoly <varkoly@suse.com>
2019-04-15 16:13:58 +0200

    Restart services using removed files before and after update.

--- sbin/oss_update.sh
+++ sbin/oss_update.sh
@@ -1,6 +1,17 @@
 DATE=$( /usr/share/oss/tools/oss_date.sh )
 . /etc/profile.d/profile.sh
+#Restart all processes which have deleted files
+for i in $( zypper ps --print "%s" )
+do
+	systemctl restart $i
+done
+
 echo "/var/log/OSS-UPDATE-$DATE" > /var/adm/oss/update-started
 zypper --no-gpg-checks --gpg-auto-import-keys -n up --auto-agree-with-licenses $@ 2>&1 | tee /var/log/OSS-UPDATE-$DATE
-rm /var/adm/oss/update-started
 
+#Restart all processes which have deleted files
+for i in $( zypper ps --print "%s" )
+do
+	systemctl restart $i
+done
+rm /var/adm/oss/update-started
