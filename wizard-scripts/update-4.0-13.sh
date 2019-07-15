16b5c741c737f51615bf207255d8992a311eeb23
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-07-15 16:53:05 +0200

    Add some magic scripts.

---
+++ wizard-scripts/update-4.0-13.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Add Access-Control-Allow-Methods to the admin server confiugartion.
+
+if [ -e "/var/adm/oss/update-4.0-13" ]
+then
+echo "Patch 4.0-13 already installed"
+        exit 0
+fi
+
+grep -q Access-Control-Expose-Headers /etc/apache2/vhosts.d/admin_include.conf || {
+   sed -i '/Access-Control-Allow-Methods/a Header always set Access-Control-Expose-Headers "Content-Disposition"' /etc/apache2/vhosts.d/admin_include.conf
+   systemctl reload apache
+}
+
+touch /var/adm/oss/update-4.0-13
+
