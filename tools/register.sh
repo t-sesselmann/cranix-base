59d99415a15147fe5af5483d147d983ad238389c
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-07-16 16:08:06 +0200

    Fix typo.

--- tools/register.sh
+++ tools/register.sh
@@ -11,7 +11,7 @@ if [ -z "${REPO_USER}" -o -z "${REPO_PASSWORD}" ]; then
 fi
 
 VALID=$( curl -X GET https://repo.cephalix.eu/api/customers/regcodes/${SCHOOL_REG_CODE} )
-if [ $? > 0 ]; then
+if [ $? -gt 0 ]; then
         echo "Can not register."
         exit 1
 fi
