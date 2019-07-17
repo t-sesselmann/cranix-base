d0bc4ae909c8d8c193727bd412c25ad6f3d36ad6
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-07-17 14:44:33 +0200

    Initial version of the migration to 4-1.

---
+++ updates/migrate-to-4-1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import json
+import os
+
+rooms=json.load(os.popen('oss_api.sh GET rooms/all'))
+for room in rooms:
+    #print room
+    f = os.popen("/usr/share/oss/plugins/add_room/100-add-firewalld.sh","w")
+    f.write("name: "+ room["name"] + "\n")
+    f.write("startip: "+ room["startIP"] + "\n")
+    f.write("netmask: %i\n" % room["netMask"] )
+    f.close
+
+#SCHOOL_NET
+network=os.popen('oss_api_text.sh GET system/configuration/NETWORK').read()
+netmask=os.popen('oss_api_text.sh GET system/configuration/NETMASK').read()
+f = os.popen("/usr/share/oss/plugins/add_room/100-add-firewalld.sh","w")
+f.write("name: SCHOOL_NET\n")
+f.write("startip: "+ network + "\n")
+f.write("netmask: "+ netmask + "\n")
+f.close
+
+#ANON_DHCP
+anon=os.popen('oss_api_text.sh GET system/configuration/ANON_DHCP_NET').read().split("/")
+f = os.popen("/usr/share/oss/plugins/add_room/100-add-firewalld.sh","w")
+f.write("name: SCHOOL_NET\n")
+f.write("startip: "+ anon[0] + "\n")
+f.write("netmask: "+ anon[1] + "\n")
+f.close
+
