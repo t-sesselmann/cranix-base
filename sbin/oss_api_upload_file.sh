9fdebe9b666c6872c242093d5f38ed62b8f946b2
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-08-09 20:14:05 +0200

    New api script to upload files.

---
+++ sbin/oss_api_upload_file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright Peter Varkoly <peter@varkoly.de>
+
+CALL=$1
+DATA=$2
+DATA=" -F file=@${DATA}"
+
+TOKEN=$( grep de.openschoolserver.api.auth.localhost= /opt/oss-java/conf/oss-api.properties | sed 's/de.openschoolserver.api.auth.localhost=//' )
+curl -s -X POST --header 'Content-Type: multipart/form-data' $DATA --header 'Authorization: Bearer '${TOKEN} "http://localhost:9080/api/$CALL"
+
