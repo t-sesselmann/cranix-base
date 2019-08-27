3796cc25a10096e36ff3d69b347e820d722df41a
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-08-27 14:52:09 +0200

    Add new wizard scripts.

---
+++ wizard-scripts/correct-adhoc-lan.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# Srcipt to correct the device if the ip address of the device is not from the room.
+# CopyRight Dipl.-Ing. Peter Varkoly <peter@varkoly.de>
+import json
+import os
+import sys
+from netaddr import *
+
+rooms=json.load(os.popen('oss_api.sh GET rooms/all'))
+
+for room in rooms:
+  id=room['id']
+  ip=room['startIP']
+  nm=room['netMask']
+  net = "{}/{}".format(ip,nm)
+  net1 = IPNetwork(net)
+  cmd = "oss_api.sh GET rooms/{}/devices".format(id)
+  devices=json.load(os.popen(cmd))
+  print net
+  for device in devices:
+    net2 = IPNetwork("{}/{}".format(device['ip'],nm))
+    if net1 != net2:
+       cmd= "oss_api.sh GET rooms/{}/availableIPAddresses".format(id)
+       freeip=json.load(os.popen(cmd))
+       print "   {} {} new ip {}".format(device['id'], device['ip'], freeip[0])
+       device['ip']=freeip[0]
+       fobj = open(device['name'],'w')
+       fobj.write(json.dumps(device))
+       fobj.close()
+       cmd= "/usr/sbin/oss_api_post_file.sh devices/forceModify " + device['name']
+       result=json.load(os.popen(cmd))
+       print result
+       nb = raw_input('OK?')
+       if nb != 'o':
+          sys.exit(1) 
+
---
+++ wizard-scripts/sync-ips-from-db-to-samba.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# Sync the ip addresses from the Devices table into the samba dns server
+# This can be neccessary if someone has made some manual changes
+# Copyright Dipl Ing Peter Varkoly <peter@varkoly.de>
+
+import json
+import os
+import sys
+import socket
+from configobj import ConfigObj
+config = ConfigObj("/opt/oss-java/conf/oss-api.properties")
+passwd = config['de.openschoolserver.dao.User.Register.Password']
+domain = os.popen('oss_api_text.sh GET system/configuration/DOMAIN').read()
+
+devices=json.load(os.popen('/usr/sbin/oss_api.sh GET devices/all'))
+for device in devices:
+  print(device)
+  try:
+    oldip=socket.gethostbyname(device['name'])
+  except:
+    if os.system("samba-tool dns add localhost " + domain + " " + device['name'] + " A " + device['ip'] + "  -U register%" + passwd ) != 0:
+      print("Can not create dns entry for " + device['name'] + " " + device['ip'] )
+    else:
+      oldip=device['ip']
+  if device['ip'] != oldip:
+    if os.system("samba-tool dns update localhost " + domain + " " + device['name'] + " A " + oldip + " " + device['ip'] + "  -U register%" + passwd ) != 0:
+      print("Can not update dns entry for " + device['name'] + " from:" + oldip + " to:" + device['ip'] )
+
+print("Recreate the reverse zone")
+
+netmask=int(os.popen('oss_api_text.sh GET system/configuration/NETMASK').read().rstrip())
+network=os.popen('oss_api_text.sh GET system/configuration/NETWORK').read().split('.')
+revdomain=""
+print netmask
+if netmask > 23:
+  revdomain = network[2]+'.'+network[1]+'.'+network[0]+'.IN-ADDR.ARPA'
+elif netmask > 15 and netmask < 24:
+  revdomain = network[1]+'.'+network[0]+'.IN-ADDR.ARPA'
+elif netmask > 7 and netmask < 16:
+  revdomain = network[0]+'.IN-ADDR.ARPA'
+
+os.system("samba-tool dns zonedelete localhost " + revdomain + " -U register%" + passwd)
+os.system("samba-tool dns zonecreate localhost " + revdomain + " -U register%" + passwd)
+for device in devices:
+  ip   = device["ip"].split('.')
+  name = device["name"]
+  if netmask > 23:
+    if ip[0] != network[0] or ip[1] != network[1] or ip[2] != network[2]:
+      next
+    rdomain = ip[3]
+  elif netmask > 15:
+    if ip[0] != network[0] or ip[1] != network[1]:
+      next
+    rdomain = ip[3]+'.'+ip[2]
+  elif netmask > 7:
+    if ip[0] != network[0]:
+      next
+    rdomain = ip[3]+'.'+ip[2]+'.'+ip[1]
+  os.system("samba-tool dns add localhost " + revdomain + " " + rdomain + " PTR " + name + "." + domain + "  -U register%" + passwd )
+
