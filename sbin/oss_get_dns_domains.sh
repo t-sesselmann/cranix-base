38366fcb86537a49635d6ae8d22cc687db90b3a4
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-03-01 07:48:22 +0100

    Ticket#2020021267001962 — DNS Management: Domain nicht mehr da

--- sbin/oss_get_dns_domains.sh
+++ sbin/oss_get_dns_domains.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-. /etc/sysconfig/schoolserver
 
-BASEDN=$( ldbsearch -H /var/lib/samba/private/sam.ldb "(&(objectClass=domain)(dc=$SCHOOL_WORKGROUP))" dn | grep dn: | sed 's/dn: //' )
-ldbsearch -H /var/lib/samba/private/sam.ldb -b CN=MicrosoftDNS,DC=DomainDnsZones,$BASEDN -s one '(!(name=RootDNSServers))' name  | grep name: | sed 's/name: //'
+passwd=$( /usr/bin/grep de.openschoolserver.dao.User.Register.Password= /opt/oss-java/conf/oss-api.properties | /usr/bin/sed 's/de.openschoolserver.dao.User.Register.Password=//' )
+/usr/bin/samba-tool dns zonelist admin -U register%${passwd} | /usr/bin/grep pszZoneName  | /usr/bin/grep -v _msdcs. | /usr/bin/gawk  '{ print $3 }'
