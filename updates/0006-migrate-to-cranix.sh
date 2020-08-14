3844d668cd85fdea9099ca0fead64f57d3884670
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-08-14 12:27:41 +0200

    Take care about setting up chrony

--- updates/0006-migrate-to-cranix.sh
+++ updates/0006-migrate-to-cranix.sh
@@ -11,11 +11,17 @@ rsync -aAv /usr/share/oss/templates/ /usr/share/cranix/templates/
 sed -i s#oss/plugins#cranix/plugins# /etc/samba/smb.conf
 sed -i s#oss/tools#cranix/tools# /etc/samba/smb.conf
 sed -i s/oss_api.sh/crx_api.sh/ /etc/sysconfig/scripts/SuSEfirewall2-custom
+if [ -e /etc/chrony.d/oss.conf ]; then
+	mv /etc/chrony.d/oss.conf /etc/chrony.d/cranix.conf
+fi
 systemctl restart atd
 
 echo "/usr/bin/zypper ref &>  /var/log/MIGRATE-TO-CRANIX-1.log
 /usr/bin/zypper -n up &>  /var/log/MIGRATE-TO-CRANIX-2.log
 /bin/rpm -e --nodeps OSS-release OSS-release-dvd &>  /var/log/MIGRATE-TO-CRANIX-3.log
 /usr/bin/zypper -n install CRANIX-release CRANIX-release-dvd &>  /var/log/MIGRATE-TO-CRANIX-4.log
+if [ ! -e /etc/chrony.d/cranix.conf ]; then
+	/usr/share/cranix/setup/scripts/setup-chrony.sh
+fi
 " |  at "now + 20 minutes"
 
