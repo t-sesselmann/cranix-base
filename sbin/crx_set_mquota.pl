a22128c7e1d6514cf397880319562a3484c092e7
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-09-17 20:53:18 +0200

    Quota 0 does not means no quota. It means no email.

--- sbin/crx_set_mquota.pl
+++ sbin/crx_set_mquota.pl
@@ -2,7 +2,7 @@
 use Mail::IMAPClient;
 use strict;
 my $user  = shift;
-my $quota = shift || 0;
+my $quota = shift || -1;
 
 my $passwd=`grep de.cranix.dao.User.Register.Password= /opt/cranix-java/conf/cranix-api.properties | sed 's/de.cranix.dao.User.Register.Password=//'`;
 chomp $passwd;
@@ -13,7 +13,9 @@ my $imap = Mail::IMAPClient->new(
   Ssl      => 0,
   Uid      => 1,
 );
-
+if( $quota == 0 ){
+	$quota = -1;
+}
 $imap->setquota("user".$imap->separator.$user,"STORAGE",$quota);
 print $imap->quota("user".$imap->separator.$user)."\n";
 print $imap->quota_usage("user".$imap->separator.$user)."\n";
