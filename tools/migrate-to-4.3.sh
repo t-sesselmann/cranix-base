872a020f6333f41114f829dde16da32960e8a3e0
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-10-22 14:35:22 +0200

    Add migrations script to 4.3

---
+++ tools/migrate-to-4.3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sed -i 's/4.2/4.3/g' /etc/zypp/repos.d/CRANIX.repo
+sed -i 's/4.2/4.3/g' /etc/zypp/credentials.cat
+sed -i 's/solver.dupAllowVendorChange.*$/solver.dupAllowVendorChange = true/' /etc/zypp/zypp.conf
+DATE=$( /usr/share/cranix/tools/crx_date.sh )
+zypper --no-gpg-checks --gpg-auto-import-keys -n dup --auto-agree-with-licenses  2>&1 | tee /var/log/CRANIX-UPDATE-$DATE
+systemctl enable samba-ad
