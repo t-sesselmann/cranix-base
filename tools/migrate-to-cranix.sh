c493b2b1bd9fc054210d68711462b9cc0a49589d
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-10-24 09:53:15 +0200

    Rejoin the printserver. Wait 10 minutes for samba to rebuild the db files.

--- tools/migrate-to-cranix.sh
+++ tools/migrate-to-cranix.sh
@@ -19,11 +19,18 @@ if [ -e /etc/chrony.d/oss.conf ]; then
 	mv /etc/chrony.d/oss.conf /etc/chrony.d/cranix.conf
 fi
 cp /usr/share/oss/tools/migrate-db-to-cranix.sh /var/adm/oss/migrate-db-to-cranix.sh
+passwd=$( grep de.openschoolserver.dao.User.Register.Password= /opt/oss-java/conf/oss-api.properties | sed 's/de.openschoolserver.dao.User.Register.Password=//' )
+echo "Rejoin printserver to AD" >> /var/adm/oss/migrate-db-to-cranix.sh
+echo "net ADS JOIN -s /etc/samba/smb-printserver.conf -U register%${passwd}" >> /var/adm/oss/migrate-db-to-cranix.sh
+
 chmod 755 /var/adm/oss/migrate-db-to-cranix.sh
 systemctl restart atd
 
 echo "/usr/bin/zypper ref &>  /var/log/MIGRATE-TO-CRANIX-1.log
-/usr/bin/zypper -n dup &>  /var/log/MIGRATE-TO-CRANIX-2.log
+/usr/bin/zypper -n dup &>  /var/log/MIGRATE-TO-CRANIX-2.log || {
+	echo "Migration TO CRANIX Failed" > /var/log/MIGRATE-TO-CRANIX-FAILED
+	exit 1
+}
 /bin/rpm -e --nodeps OSS-release-dvd &>  /var/log/MIGRATE-TO-CRANIX-3.log
 cd /etc/products.d
 rm baseproduct
@@ -32,8 +39,11 @@ cd
 if [ ! -e /etc/chrony.d/cranix.conf ]; then
 	/usr/share/cranix/setup/scripts/setup-chrony.sh
 fi
-/usr/bin/systemctl restart samba-ad
+/usr/bin/systemctl enable samba-ad
+/usr/bin/systemctl start  samba-ad
+sleep 600
 /var/adm/oss/migrate-db-to-cranix.sh
 /usr/lib/systemd-presets-branding/branding-preset-states save
+reboot
 " |  at "now + 5 minutes"
 
