d80f008239f9377b4385fd3dbc396de7c4f1db8a
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-11-05 20:01:05 +0100

    Adap /etc/sysconfig/cranix

--- tools/migrate-to-4.3.sh
+++ tools/migrate-to-4.3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sed -i 's/4.2/4.3/g' /etc/zypp/repos.d/CRANIX.repo
 sed -i 's/4.2/4.3/g' /etc/zypp/credentials.cat
+sed -i 's/oss_/crx_/g' /etc/sysconfig/cranix
+sed -i 's/cranix_get_screenshots/crx_get_screenshots/' /etc/sysconfig/cranix
+sed -i 's/samba /samba-ad /' /etc/sysconfig/cranix
 sed -i 's/solver.dupAllowVendorChange.*$/solver.dupAllowVendorChange = true/' /etc/zypp/zypp.conf
+zypper ref
 DATE=$( /usr/share/cranix/tools/crx_date.sh )
 zypper --no-gpg-checks --gpg-auto-import-keys -n dup --auto-agree-with-licenses  2>&1 | tee /var/log/CRANIX-UPDATE-$DATE
 systemctl enable samba-ad
