dcc7960304c5b44ce396554751fa7a8f1df3aa95
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-11-05 08:02:57 +0100

    Make some changes based on somne experiences

--- tools/migrate-to-cranix.sh
+++ tools/migrate-to-cranix.sh
@@ -16,13 +16,14 @@ sed -i s#oss/plugins#cranix/plugins# /etc/samba/smb.conf
 sed -i s#oss/tools#cranix/tools# /etc/samba/smb.conf
 sed -i s/oss_api.sh/crx_api.sh/ /etc/sysconfig/scripts/SuSEfirewall2-custom
 sed -i 's#/usr/share/.*squid_sso.py#/usr/share/cranix/tools/squid_sso.py#'  /etc/squid/squid.conf
+zypper mr -G -p 1 CRANIX
 
 if [ -e /etc/chrony.d/oss.conf ]; then
 	mv /etc/chrony.d/oss.conf /etc/chrony.d/cranix.conf
 fi
 cp /usr/share/oss/tools/migrate-db-to-cranix.sh /var/adm/oss/migrate-db-to-cranix.sh
 passwd=$( grep de.openschoolserver.dao.User.Register.Password= /opt/oss-java/conf/oss-api.properties | sed 's/de.openschoolserver.dao.User.Register.Password=//' )
-echo "Rejoin printserver to AD" >> /var/adm/oss/migrate-db-to-cranix.sh
+echo "#Rejoin printserver to AD" >> /var/adm/oss/migrate-db-to-cranix.sh
 echo "net ADS JOIN -s /etc/samba/smb-printserver.conf -U register%${passwd}" >> /var/adm/oss/migrate-db-to-cranix.sh
 
 chmod 755 /var/adm/oss/migrate-db-to-cranix.sh
@@ -34,6 +35,9 @@ then
 	echo "Migration TO CRANIX Failed" > /var/log/MIGRATE-TO-CRANIX-FAILED
 	exit 1
 fi
+/usr/bin/systemctl enable samba-ad
+/usr/bin/systemctl start  samba-ad
+sleep 600
 /bin/rpm -e --nodeps OSS-release-dvd &>  /var/log/MIGRATE-TO-CRANIX-3.log
 cd /etc/products.d
 rm baseproduct
@@ -42,11 +46,8 @@ cd
 if [ ! -e /etc/chrony.d/cranix.conf ]; then
 	/usr/share/cranix/setup/scripts/setup-chrony.sh
 fi
-/usr/bin/systemctl enable samba-ad
-/usr/bin/systemctl start  samba-ad
-sleep 600
 /var/adm/oss/migrate-db-to-cranix.sh
 /usr/lib/systemd-presets-branding/branding-preset-states save
 reboot
-" |  at "now + 5 minutes"
+" |  at "now + 1 minute"
 
