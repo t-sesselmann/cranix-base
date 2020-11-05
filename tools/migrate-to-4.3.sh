0a689eea4abac6598cd8b3400d93683363b9d811
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-11-05 08:57:00 +0100

    Start samba-ad after migration wait for rebuilding the db-files and reboot

--- tools/migrate-to-4.3.sh
+++ tools/migrate-to-4.3.sh
@@ -5,3 +5,6 @@ sed -i 's/solver.dupAllowVendorChange.*$/solver.dupAllowVendorChange = true/' /e
 DATE=$( /usr/share/cranix/tools/crx_date.sh )
 zypper --no-gpg-checks --gpg-auto-import-keys -n dup --auto-agree-with-licenses  2>&1 | tee /var/log/CRANIX-UPDATE-$DATE
 systemctl enable samba-ad
+systemctl start samba-ad
+sleep 600
+/sbin/reboot
