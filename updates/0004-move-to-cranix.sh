87c37fff6f58b61b562803ab5e3e0ac0ac26190b
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-02-02 18:58:17 +0100

    Set CRANIX_MAINTAIN_ADHOC_ROOM_FOR_CLASSES default no

--- updates/0004-move-to-cranix.sh
+++ updates/0004-move-to-cranix.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mv /etc/sysconfig/schoolserver /etc/sysconfig/cranix
-sed -i s/SCHOOL_/CRANIX_/g /etc/sysconfig/cranix
-sed -i s/OSS/CRANIX/ /etc/sysconfig/cranix
-#Remove old link
-rm /srv/salt/repos.d/OSS.repo
-#Move customized files to the right place
-rsync -av /usr/share/oss/ /usr/share/cranix/
+if [ -e /etc/sysconfig/schoolserver ]; then
+	mv /etc/sysconfig/schoolserver /etc/sysconfig/cranix
+	sed -i s/SCHOOL_/CRANIX_/g /etc/sysconfig/cranix
+	sed -i s/OSS/CRANIX/ /etc/sysconfig/cranix
+	#Remove old link
+	rm /srv/salt/repos.d/OSS.repo
+	#Move customized files to the right place
+	rsync -av /usr/share/oss/ /usr/share/cranix/
+fi
