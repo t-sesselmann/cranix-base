757533f7b7af956f115a3e16d03697941442f374
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-03-25 09:04:57 +0100

    Take care about cephalix clients too.

--- updates/0005-fix-apache-allowed-methodes.sh
+++ updates/0005-fix-apache-allowed-methodes.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sed -i 's/Header always set Access-Control-Allow-Methods.*/Header always set Access-Control-Allow-Methods "POST, GET, OPTIONS, DELETE, PUT, PATCH"/' /etc/apache2/vhosts.d/admin_include.conf
-/usr/bin/systemctl reload apache2
+sed -i 's/Header always set Access-Control-Allow-Methods.*/Header always set Access-Control-Allow-Methods "POST, GET, OPTIONS, DELETE, PUT, PATCH"/' \
+	/etc/apache2/vhosts.d/admin_include.conf
 
+if [ -e /etc/apache2/vhosts.d/cephalix_include.conf ]
+then
+	sed -i 's/Header always set Access-Control-Allow-Methods.*/Header always set Access-Control-Allow-Methods "POST, GET, OPTIONS, DELETE, PUT, PATCH"/' \
+		/etc/apache2/vhosts.d/cephalix_include.conf
+fi
+
+/usr/bin/systemctl reload apache2
