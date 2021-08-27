50a3fca19a3b8530a616943dfdc5de8812a694f4
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-08-27 09:08:32 +0200

    Implementing creating archiv of pdf also. Remove existing archive befor creating new one.

--- tools/pack_import.sh
+++ tools/pack_import.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# (C) 2021 Peter Varkoly <pvarkoly@cephalix.eu>
 
 import=$1
+itype=$2
+if [ -z "${itype}" ]; then
+        itype="txt"
+fi
+
 if [ -d  /home/groups/SYSADMINS/userimports/${import} ]
 then
         cd /home/groups/SYSADMINS/userimports/${import}
-        zip userimport.zip *txt
+        rm -f userimport.zip
+        zip userimport.zip *.${itype}
 fi
