126288d6c7b83d0f996c6cba2a9ead3a9edb181a
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-12-11 20:47:57 +0100

    Use offline commands. By migration firewall can not work.

--- tools/sync-rooms-to-firewalld.py
+++ tools/sync-rooms-to-firewalld.py
@@ -4,7 +4,8 @@ import os
 
 rooms=json.load(os.popen('crx_api.sh GET rooms/all'))
 for room in rooms:
-  p = os.popen("/usr/share/cranix/plugins/add_room/100-add-firewalld.sh","w")
-  p.write("name: {}\nstartip: {}\nnetmask: {}\n".format(room['name'],room['startIP'],room['netMask']))
-  p.close()
+  os.system('firewall-offline-cmd --new-zone={0}'.format(room['name']))
+  os.system('firewall-offline-cmd --zone={0} --set-description="Zone for Room {0}"'.format(room['name']))
+  os.system('firewall-offline-cmd --zone={0} --add-source="{1}/{2}"'.format(room['name'],room['startIP'],room['netMask']))
+  os.system('firewall-offline-cmd --zone={0} --set-target=ACCEPT'.format(room['name']))
 
