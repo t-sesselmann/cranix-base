bd10dcfa23231f1f881a4eb62a713142a606f0ca
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-01-20 08:52:12 +0100

    Only readd not existing rooms. Only reload firewalld if there were some missed rooms

--- tools/sync-rooms-to-firewalld.py
+++ tools/sync-rooms-to-firewalld.py
@@ -5,6 +5,7 @@ import os
 import re
 
 zones = []
+to_reload = False
 for line in os.popen('/usr/bin/firewall-offline-cmd --list-all-zones').readlines():
     match1 = re.search("^(\S+)",line)
     if match1:
@@ -12,8 +13,11 @@ for line in os.popen('/usr/bin/firewall-offline-cmd --list-all-zones').readlines
 
 for room in json.load(os.popen('crx_api.sh GET rooms/all')):
     if room['name'] not in zones:
-        os.system('firewall-offline-cmd --new-zone={0}'.format(room['name']))
-        os.system('firewall-offline-cmd --zone={0} --set-description="Zone for Room {0}"'.format(room['name']))
-        os.system('firewall-offline-cmd --zone={0} --add-source="{1}/{2}"'.format(room['name'],room['startIP'],room['netMask']))
-        os.system('firewall-offline-cmd --zone={0} --set-target=ACCEPT'.format(room['name']))
+        os.system('/usr/bin/firewall-offline-cmd --new-zone={0}'.format(room['name']))
+        os.system('/usr/bin/firewall-offline-cmd --zone={0} --set-description="Zone for Room {0}"'.format(room['name']))
+        os.system('/usr/bin/firewall-offline-cmd --zone={0} --add-source="{1}/{2}"'.format(room['name'],room['startIP'],room['netMask']))
+        os.system('/usr/bin/firewall-offline-cmd --zone={0} --set-target=ACCEPT'.format(room['name']))
+        to_reload = True
+if to_reload:
+    os.system('/usr/bin/firewall-cmd --reload')
 
