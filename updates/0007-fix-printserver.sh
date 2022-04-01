f11d962721c995daf7eecf78f930ca42f4d0dbbf
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-04-01 17:48:46 +0200

    Fix printserver configuration

---
+++ updates/0007-fix-printserver.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+if [ -e /var/adm/cranix/migrate-4-4/printserver-fixed ]; then
+	exit
+fi
+. /etc/sysconfig/cranix
+
+IPPRINTSERVER=$( host printserver | gawk '{ print $4 }' )
+IPPRSQL=$( echo  "select IP from Devices where name='printserver'" | mysql CRX | tail -n 1 )
+NEXTIP=$( crx_api.sh GET rooms/1/availableIPAddresses | jq .[1] | sed 's/"//g' )
+DEVINT=$( grep -l $CRANIX_SERVER /etc/sysconfig/network/ifcfg-* )
+
+if [ -z "${IPPRSQL}" ]
+then
+	#Original 4.4 There is nothing with printserver
+	echo "INSERT INTO Devices VALUES(4,1,1,NULL,'printserver','${NEXTIP}',NULL,'','',0,0,'','','',0);" | msql CRX
+	/usr/bin/systemctl restart cranix-api.service 
+	sed -i "s/172.28.0.4/${NEXTIP}/" /etc/sysconfig/cranix
+	/usr/sbin/crx_add_host.sh printserver ${NEXTIP}
+	echo "IPADDR_print='${NEXTIP}/${CRANIX_NETMASK}'
+LABEL_print='print'" >> ${DEVINT}
+	ip addr add ${NEXTIP}/${CRANIX_NETMASK} dev ${DEVINT/*ifcfg-/}
+	/usr/share/cranix/setup/scripts/crx-setup.sh --printserver
+elif [ "${IPPRINTSERVER}" = ${CRANIX_SERVER} ]
+then
+	/usr/sbin/crx_update_host.sh printserver ${IPPRINTSERVER} ${IPPRSQL}
+	sed -i "s/CRANIX_PRINTSERVER=.$/CRANIX_PRINTSERVER=\"${IPPRSQL}\"/" /etc/sysconfig/cranix
+	echo "IPADDR_print='${IPPRSQL}/${CRANIX_NETMASK}'
+LABEL_print='print'" >> ${DEVINT}
+	ip addr add ${IPPRSQL}/${CRANIX_NETMASK} dev ${DEVINT/*ifcfg-/}
+else
+	SUPPORT='{"email":"noreply@cephalix.eu","subject":"Can not fix printserver configuration","description":"Can not fix printserver configuration","regcode":"'${CRANIX_REG_CODE}'"}'
+        curl -s -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${SUPPORT}" ${CRANIX_SUPPORT_URL}
+	exit 1
+fi
+/usr/share/cranix/tools/sync-ptrrecords-to-samba.py
+/usr/share/cranix/tools/sync-cups-to-samba.py
+/usr/sbin/crx_manage_room_access.py --all --set_defaults
+/usr/bin/systemctl start samba-printserver
+/usr/bin/systemctl enable samba-printserver
+
+mkdir -p /var/adm/cranix/migrate-4-4/
+touch /var/adm/cranix/migrate-4-4/printserver-fixed
