84a312e253a240517a14d601288d571812dd07cd
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-05-04 11:42:45 +0200

    Add new services and remove old services from system configuration

--- tools/fix-sysconfig.py
+++ tools/fix-sysconfig.py
@@ -3,6 +3,23 @@
 import os
 from configobj import ConfigObj
 
+# new services to add
+new_services = (
+        'chronyd',
+        'samba-ad',
+        'samba-fileserver',
+        'samba-printserver',
+        'cranix-api',
+        'crx_salt_event_watcher'
+        )
+# old services to remove
+old_services = (
+        'ntpd',
+        'samba',
+        'oss-api',
+        'oss_salt_event_watcher'
+        )
+
 #Create backup directory
 backup_dir = '/var/adm/cranix/backup/{0}'.format(os.popen('/usr/share/cranix/tools/crx_date.sh').read()).strip()
 os.system('mkdir -p {0}'.format(backup_dir))
@@ -14,6 +31,15 @@ cranix_conf = ConfigObj('/etc/sysconfig/cranix',list_values=False,encoding='utf-
 for key in fillup_template:
     if key in cranix_conf:
         fillup_template[key] = cranix_conf[key]
+services=cranix_conf['CRANIX_MONITOR_SERVICES'].strip('"').strip("'").split()
+for i in old_services:
+    if i in services:
+        services.remove(i)
+for i in new_services:
+    if i not in services:
+        services.append(i)
+services.sort()
+fillup_template['CRANIX_MONITOR_SERVICES']="'" + ' '.join(services) + "'"
 fillup_template.filename = '/etc/sysconfig/cranix'
 fillup_template.write()
 
