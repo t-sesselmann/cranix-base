a123b223107f73f9047cb86ed53082b0d74f49c2
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-09-08 09:22:20 +0200

    Fix update script

--- updates/0010-adapt-atd-settings.sh
+++ updates/0010-adapt-atd-settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . /etc/sysconfig/atd
-if [ -z ${ATD_OPTIONS} ]; then
+if [ -z "${ATD_OPTIONS}" ]; then
 	sed -i 's/ATD_OPTIONS=.*/ATD_OPTIONS="-b 20"/' /etc/sysconfig/atd
 	/usr/bin/systemctl restart atd
 fi
