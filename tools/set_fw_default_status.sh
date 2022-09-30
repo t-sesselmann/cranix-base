138137c9c8afa3ed92f1d5364e3f21a513a53601
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2022-09-30 11:29:57 +0200

    Now it is possible to execute custom script when starting the firewall

--- tools/set_fw_default_status.sh
+++ tools/set_fw_default_status.sh
@@ -3,4 +3,7 @@
 /usr/share/cranix/tools/wait-for-api.sh
 /usr/share/cranix/tools/remove-internal-device-from-firewall.sh
 /usr/sbin/crx_manage_room_access.py --all --set_defaults
+if [ -e /usr/share/cranix/tools/custom/fw-post-start.sh ]; then
+        /usr/share/cranix/tools/custom/fw-post-start.sh
+fi
 
